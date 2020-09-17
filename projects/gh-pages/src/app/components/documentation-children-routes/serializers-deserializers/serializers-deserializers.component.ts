4e4fe0b24676f4ea841a3f079e088d1ed5abf309
Ernesto Pedrosa Alonso <ernestop891123@gmail.com>
2020-09-17 23:11:40 +0200

    Update the documentation with the changes of the v2 of ng-ui2qp #23
    - Reviewed the redaction and updated the documentation in some pages.
    - Added some styles.

--- projects/gh-pages/src/app/components/documentation-children-routes/serializers-deserializers/serializers-deserializers.component.ts
+++ projects/gh-pages/src/app/components/documentation-children-routes/serializers-deserializers/serializers-deserializers.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
+import {Deserializer, Serializer, Ui2QpDeserializersService, Ui2QpSerializersService} from 'ng-ui2qp';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-serializers-deserializers',
@@ -8,57 +10,127 @@ import {Title} from '@angular/platform-browser';
 })
 export class SerializersDeserializersComponent implements OnInit {
 
+  datePickerControl = new FormControl();
+
   serializerTypeCode = `{
-      type: string;
-      serializerFunc: (value: any) => string;
-    }`;
+  type: string;
+  serializerFunc: (value: any) => string;
+}`;
 
   stringSerializerCode = `{
-      type: 'string',
-      serializerFunc: (value: any) => {
-        return value;
-      },
-    }`;
+  type: 'string',
+  serializerFunc: (value: any) => {
+    return value;
+  },
+}`;
 
   deserializerTypeCode = `{
-      type: string;
-      deserializerFunc: (value: any, defaultValue: any) => any;
-    }`;
+  type: string;
+  deserializerFunc: (value: any, defaultValue: any) => any;
+}`;
 
   numberDeserializerCode = `{
-      type: 'number',
-      deserializerFunc: (value: any, defaultVal: any) => {
-       // tslint:disable-next-line:radix
-       const returnValue = parseInt(value);
-       if (typeof returnValue !== 'number') {
-          console.error("The value couldn't be deserialized, instead we used the default value");
-          return defaultVal;
-        }
-       return returnValue;
-      },
-    }`;
+  type: 'number',
+  deserializerFunc: (value: any, defaultVal: any) => {
+    const returnValue = parseInt(value);
+    if (typeof returnValue !== 'number') {
+      console.error("The value couldn't be deserialized, instead we used the default value");
+      return defaultVal;
+    }
+    return returnValue;
+  },
+}`;
 
   customDeserializerCode = `//Assuming that the deserializer service was injected
-    this.deserializerService.register({
-    type: 'datetime-picker', // Chosen type
-    deserializerFunc: (value: string, defaultValue: Date) => {
-        return value !== null && value !== '' ? new Date(value) : '';
-      }
-    });`;
+this.deserializerService.register({
+  type: 'datetime-picker', // Chosen type
+  deserializerFunc: (value: string, defaultValue: Date) => {
+    return value !== null && value !== '' ? new Date(value) : '';
+  }
+});`;
 
   customSerializerCode = `//Assuming that the serializer service was injected
-    this.serializerService.register({
-    type: 'datetime-picker', // Chosen type
-    serializerFunc: (value: Date) => {
+this.serializerService.register({
+  type: 'datetime-picker', // Chosen type
+  serializerFunc: (value: Date) => {
+    return value !== null && value !== undefined ? value.toISOString() : null;
+  }
+});`;
+
+  codeExample = {
+    html: `<mat-form-field appearance="fill">
+    <mat-label>Choose a date</mat-label>
+    <input matInput [matDatepicker]="picker" [formControl]="datePickerControl" [ui2qpControl]="{type:'datetime-picker', qpName:'date'}">
+    <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
+    <mat-datepicker #picker></mat-datepicker>
+</mat-form-field>`,
+    ts: `import {Deserializer, Serializer, Ui2QpDeserializersService, Ui2QpSerializersService} from 'ng-ui2qp';
+...
+@Component({
+...
+})
+export class FullExampleComponent implements OnDestroy{
+
+  datePicker: FormControl = new FormControl();
+
+  constructor(private serializersService: Ui2QpSerializersService,
+              private deserializersService: Ui2QpDeserializersService) {
+    const dateTimePickerType = 'datetime-picker';
+    // defining a custom serializer for the datetime-picker
+    const datetimePickerSerializer: Serializer = {
+      type: dateTimePickerType,
+      serializerFunc: (value: Date) => {
         return value !== null && value !== undefined ? value.toISOString() : null;
       }
-    });`;
+    };
 
-  constructor(private title: Title) {
+    this.serializersService.register(datetimePickerSerializer);
+
+    // defining a custom deserializer for the datetime-picker
+    const datetimePickerDeserializer: Deserializer = {
+      type: dateTimePickerType,
+      deserializerFunc: (value: string) => {
+        return new Date(value);
+      }
+    };
+
+    this.deserializersService.register(datetimePickerDeserializer);
+  }
+}`,
+    css: `Nothing to see here :)`
+  };
+
+  constructor(private title: Title, private serializersService: Ui2QpSerializersService,
+              private deserializersService: Ui2QpDeserializersService) {
     this.title.setTitle('ng-ui2qp | Serializers and Deserializers');
+    this.exampleCodeInit();
   }
 
   ngOnInit(): void {
+  }
+
+  exampleCodeInit() {
+
+    const dateTimePickerType = 'datetime-picker';
+    // defining a custom serializer for the datetime-picker
+    const datetimePickerSerializer: Serializer = {
+      type: dateTimePickerType,
+      serializerFunc: (value: Date) => {
+        return value !== null && value !== undefined ? value.toISOString() : null;
+      }
+    };
+
+    this.serializersService.register(datetimePickerSerializer);
+
+    // defining a custom deserializer for the datetime-picker
+    const datetimePickerDeserializer: Deserializer = {
+      type: dateTimePickerType,
+      deserializerFunc: (value: string) => {
+        return new Date(value);
+      }
+    };
+
+    this.deserializersService.register(datetimePickerDeserializer);
   }
 
 }
--- projects/gh-pages/src/app/components/documentation-children-routes/template-driven/template-driven.component.ts
+++ projects/gh-pages/src/app/components/documentation-children-routes/template-driven/template-driven.component.ts
@@ -13,15 +13,14 @@ export class TemplateDrivenComponent implements OnInit {
 
 
   withConfigCode = `import { NgUi2Qp } from '@ng-ui2qp';
-
-    @NgModule({
-        imports: [
-            NgUi2Qp.withConfig({
-                // Provide configuration here
-            }),
-        ],
-    })
-    export class YourModule {}`;
+@NgModule({
+  imports: [
+    NgUi2Qp.withConfig({
+      // Provide configuration here
+    }),
+    ],
+})
+export class YourModule {}`;
 
   controlDirectiveNativeInputExampleCode = {
     html: `<select [ui2qpControl]="{type:'number-array', qpName:'options'}" multiple [formControl]="options">
@@ -39,48 +38,48 @@ export class TemplateDrivenComponent implements OnInit {
   }`,
   };
 
-  controlDirectiveWithCVAExampleCode = `<control-value-accessor [ui2qpControl]="{qpName:'customOptions', type:'string-array'}>
-</control-value-accessor>`;
+  controlDirectiveWithCVAExampleCode = `<component-with-control-value-accessor [ui2qpControl]="{qpName:'cva', type:'number'}>
+</component-with-control-value-accessor>`;
 
   controlDirectiveWithoutCVAExampleCode = `<input type="checkbox" [ui2qpControl]="{qpName:'check', type:'boolean'}"
        ui2QpValueAccessor #accessor="controlValueAccessor" (controlValueChange)="ctrl.value = $event"
        #ctrl (input)="accessor.notifyChange($event.target.value)">`;
 
   fullExampleCode = {
-    html: `// Custom component example
-      <mat-form-field appearance="fill">
-        <mat-label>Choose a date</mat-label>
-        <input matInput [matDatepicker]="picker" [ui2qpControl]="{qpName:'birthday'}">
-        <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
-        <mat-datepicker #picker></mat-datepicker>
-      </mat-form-field>
+    html: `// Custom component example using Material Design radio buttons
+<mat-form-field appearance="fill">
+    <mat-label>Choose a date</mat-label>
+    <input matInput [matDatepicker]="picker" [ui2qpControl]="{qpName:'birthday'}">
+    <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
+    <mat-datepicker #picker></mat-datepicker>
+</mat-form-field>
 
-      // Input without ControlValueAccessor
-      <mat-form-field>
-        <mat-label for="">First Name</mat-label>
-        <input matInput type="checkbox" [ui2qpControl]="{qpName:'checked', type:'boolean'}"
-               ui2QpValueAccessor #accessor2="controlValueAccessor" (controlValueChange)="ctrl2.value = $event"
-               #ctrl2 (input)="accessor2.notifyChange($event.target.value)">
-      </mat-form-field>
+// Example of using Ui2qpValueAccessorDirective on a native input.
+<mat-form-field>
+    <mat-label for="">First Name</mat-label>
+    <input matInput type="checkbox" [ui2qpControl]="{qpName:'checked', type:'boolean'}"
+        ui2QpValueAccessor #accessor2="controlValueAccessor" (controlValueChange)="ctrl2.value = $event"
+        #ctrl2 (input)="accessor2.notifyChange($event.target.value)">
+</mat-form-field>
 
-      // Native input without ControlValueAccessor. Approach about can also be used here
-      <mat-form-field>
-        <mat-label for="">Last Name</mat-label>
-        <select matInput name="select" [ui2qpControl]="{type:'number-array', qpName:'options'}" multiple [formControl]="options">
-          <option value="">Empty</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-      </mat-form-field>`,
+//This example binds a FormControl instance to a native input as a way of providing it with a ControlValueAccessor implementation.
+<mat-form-field>
+    <mat-label for="">Last Name</mat-label>
+    <select matInput name="select" [ui2qpControl]="{type:'number-array', qpName:'options'}" multiple [formControl]="options">
+        <option value="">Empty</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+    </select>
+</mat-form-field>`,
     ts: `@Component({
-    ...
-  })
-  export class ExampleComponent {
-    options = new FormControl();
-    ...
-  }`
-  };
+...
+})
+export class ExampleComponent {
+  options = new FormControl();
+  ...
+}`
+};
 
   constructor(private title: Title) {
     this.title.setTitle('ng-ui2qp | Template-Driven');
