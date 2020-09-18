5f5c1c34cae86a21d5525fbe870f59e62ca81e0c
Ernesto Pedrosa Alonso <ernestop891123@gmail.com>
2020-09-18 16:20:23 +0200

    Removed unused projects in the repository

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'lzy-load',
-    loadChildren: () =>
-      import('./lazy-modules/lzy-load-content/lzy-load-content.module').then(
-        (m) => m.LazyLzyLoadContentModule
-      ),
-  },
-  {
-    path: 'ng-ui2qp',
-    loadChildren: () =>
-      import('./lazy-modules/ng-ui2qp/lazy-ui2qp.module').then(
-        (m) => m.LazyUi2QpModule
-      ),
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
